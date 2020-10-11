df370c4cbc449ec415a7aace706a4376f5329c3b
Yitzhak A <yitzhak.alvarez1998@yahoo.com>
2020-10-10 23:54:24 -0400

    vue works

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'buefy/dist/buefy.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
 Vue.config.productionTip = false
 
 new Vue({
