1b271fc29a05947babf8e5483bd14528c59ac38b
Yitzhak A <yitzhak.alvarez1998@yahoo.com>
2020-10-14 19:46:59 -0400

    10/14 changes

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>Features</h2>
+    <ul>
+      <li>Logging in</li>
+      <li>Exercise log</li>
+      <li>Sharin' with friends</li>
+    </ul>
   </div>
 </template>
