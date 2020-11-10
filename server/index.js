a7b6aeabb3449f02b0872c1b9988a8a6a5182b5c
Yitzhak A <yitzhak.alvarez1998@yahoo.com>
2020-11-10 10:21:29 -0500

    11/10

--- server/index.js
+++ server/index.js
@@ -1,27 +1,11 @@
 const express = require('express')
-require('dotenv').config();
-
-
+const path = require('path');
 const app = express()
-const port = process.env.PORT || 3000;
+const port = 3000
 
-console.log(process.env.BEST_CLASS);
-
-app.use(express.json());
-
-app.use(function(req, res, next) {
-  const arr = (req.headers.authorization || "").split(" ");
-  if(arr.length > 1 && arr[1] != null){
-      req.userId = +arr[1];
-  }
-  next();
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/register.html'));
 });
-
-
-app.use( (err, req, res, next) =>{
-    console.log(err);
-    res.status(err.status || 500).send( { message: err.message } )
-} )
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
