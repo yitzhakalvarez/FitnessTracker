853ac8cd3f6ec6fa5de134c13d3db695651b18e8
Yitzhak A <yitzhak.alvarez1998@yahoo.com>
2020-11-09 19:59:26 -0500

    commit

--- server/index.js
+++ server/index.js
@@ -1,11 +1,33 @@
 const express = require('express')
-const path = require('path');
-const app = express()
-const port = 3000
+require('dotenv').config();
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/signup.html'));
+const users = require('./controllers/users');
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+console.log(process.env.BEST_CLASS);
+
+app.use(express.json());
+
+app.use(function(req, res, next) {
+  const arr = (req.headers.authorization || "").split(" ");
+  if(arr.length > 1 && arr[1] != null){
+      req.userId = +arr[1];
+  }
+  next();
 });
+
+app.get('/', (req, res, next) => {
+  res.send('Hello Hudson Valley! You requested ' + req.url)
+})
+
+app.use('/users', users);
+
+app.use( (err, req, res, next) =>{
+    console.log(err);
+    res.status(err.status || 500).send( { message: err.message } )
+} )
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
