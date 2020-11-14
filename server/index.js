870affa9a26be475113cb2eb095557737c2ce99d
Yitzhak A <yitzhak.alvarez1998@yahoo.com>
2020-11-14 13:50:05 -0500

    Changes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
-const express = require('express')
-const path = require('path');
-
+require('dotenv').config();
+const express = require('express');
+const users = require('./controllers/users');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+app.use(users);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
