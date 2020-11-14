1c57ad14fcd7b6a8f05285728776365f9bb3f066
Yitzhak A <yitzhak.alvarez1998@yahoo.com>
2020-11-14 13:56:04 -0500

    Changes

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,20 +1,75 @@
+/* B"H
+*/
+
 const express = require('express');
-const users = require('../models/Users');
+const users = require('../models/users');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    users.getAll().then(x => res.send(x))
+router
+    .get('/', (req, res, next) => {
+        users.getAll().then(x=> res.send( x.map(user=> ({ ...user, Password: undefined}) ) ) )
         .catch(next);
-})
+    })
+    .get('/:id', (req, res, next) => {
+        const id = +req.params.id;
+        if(!id) return next();
+        users.get(id).then(x=> res.send( x ) )
+        .catch(next);
+    })
+    .get('/types', (req, res, next) => {
+        users.getTypes().then(x=> res.send( x ) )
+        .catch(next);
+    })
     .get('/search', (req, res, next) => {
-        users.search(req.query.q).then(x => res.send(x))
-            .catch(next);
+        users.search(req.query.q).then(x=> res.send( x ) )
+        .catch(next);
     })
     .post('/', (req, res, next) => {
-        users.add(req.query.name, req.query.age).then(newUser => {
-            res.send(newUser);
+        users.add(
+            req.body.FirstName,
+            req.body.LastName, 
+            req.body.DOB, 
+            req.body.Password, 
+            users.Types.USER, 
+        ).then(newUser => {
+            res.send( newUser );
         }).catch(next)
     })
-
+    .post('/register', (req, res, next) => {
+        users.register(
+            req.body.FirstName,
+            req.body.LastName, 
+            req.body.DOB, 
+            req.body.Password, 
+            users.Types.USER,
+            req.body.Email 
+        ).then(newUser => {
+            res.send( { ...newUser, Password: undefined } );
+        }).catch(next)
+    })
+    .post('/login', (req, res, next) => {
+        users.login(
+            req.body.email,
+            req.body.password
+        ).then(newUser => {
+            res.send( { ...newUser, Password: undefined } );
+        }).catch(next)
+    })
+   .put('/:id', (req, res, next) => {
+        users.update( req.params.id,
+            req.body.FirstName,
+            req.body.LastName, 
+            req.body.DOB, 
+            req.body.Password, 
+            users.Types.USER, 
+        ).then(newUser => {
+            res.send( newUser );
+        }).catch(next)
+    })
+    .delete('/:id', (req, res, next) => {
+        users.remove(req.params.id).then(msg => {
+            res.send( msg );
+        }).catch(next)
+    })
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
-require('dotenv').config();
 const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
 const users = require('./controllers/users');
+
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(users);
+console.log(process.env.BEST_CLASS);
 
+//  Middleware
+app.use(express.json());
+app.use(express.static( __dirname + '/../docs/'))
+
+
+
+//Init
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
