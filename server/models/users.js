a078b474a05ce6e994a9b1a510f77d859864a354
Yitzhak A <yitzhak.alvarez1998@yahoo.com>
2020-11-23 22:39:46 -0500

    fixed variables

--- server/models/users.js
+++ server/models/users.js
@@ -10,7 +10,7 @@ async function getAll(){
     //throw { status: 501, message: "This is a fake error" }
     //await Promise.resolve()
     console.log("Called Get All")
-    return await mysql.query(`SELECT * FROM ${PREFIX}Users`);
+    return await db.query(`SELECT * FROM ${PREFIX}Users`);
 }
 
 async function get(id){
@@ -18,7 +18,7 @@ async function get(id){
         *,
         (SELECT Value FROM ${PREFIX}ContactMethods Where User_id = ${PREFIX}Users.id AND Type='${cm.Types.EMAIL}' AND IsPrimary = true) as PrimaryEmail
     FROM ${PREFIX}Users WHERE id=?`;
-    const rows = await mysql.query(sql, [id]);
+    const rows = await db.query(sql, [id]);
     if(!rows.length) throw { status: 404, message: "Sorry, there is no such user" };
     return rows[0];
 }
@@ -26,7 +26,7 @@ async function get(id){
 async function login(email, password){
     const sql = `SELECT *
     FROM ${PREFIX}Users U Join ${PREFIX}ContactMethods CM ON U.id=CM.User_id WHERE CM.Value=?`;
-    const rows = await mysql.query(sql, [email]);
+    const rows = await db.query(sql, [email]);
     if(!rows.length) throw { status: 404, message: "Sorry, that email address is not registered with us." };
     console.log({password, Password: rows[0].Password});
 
@@ -38,24 +38,24 @@ async function login(email, password){
 }
 
 async function getTypes(){
-    return await mysql.query(`SELECT id, Name FROM ${PREFIX}Types WHERE Type_id = 2`);
+    return await db.query(`SELECT id, Name FROM ${PREFIX}Types WHERE Type_id = 2`);
 }
 
 async function add(FirstName, LastName, DOB, Password, User_Type){
     const sql = `INSERT INTO ${PREFIX}Users (created_at, FirstName, LastName, DOB, Password, User_Type) VALUES ? ;`;
     const params = [[new Date(), FirstName, LastName, new Date(DOB), Password, User_Type]];
-    return await mysql.query(sql, [params]);
+    return await db.query(sql, [params]);
 }
 
 async function update(id, FirstName, LastName, DOB, Password, User_Type){
     const sql = `UPDATE ${PREFIX}Users SET ? WHERE id = ?;`;
     const params = { FirstName, LastName, DOB: new Date(DOB), Password, User_Type };
-    return await mysql.query(sql, [params, id]);
+    return await db.query(sql, [params, id]);
 }
 
 async function remove(id){
     const sql = `DELETE FROM ${PREFIX}Users WHERE id = ?`;
-    return await mysql.query(sql, [id]);
+    return await db.query(sql, [id]);
 }
 
 async function register(FirstName, LastName, DOB, Password, User_Type, email) {
@@ -69,6 +69,6 @@ async function register(FirstName, LastName, DOB, Password, User_Type, email) {
     return user;
 }
 
-const search = async q => await mysql.query(`SELECT id, FirstName, LastName FROM ${PREFIX}Users WHERE LastName LIKE ? OR FirstName LIKE ?; `, [`%${q}%`, `%${q}%`]);
+const search = async q => await db.query(`SELECT id, FirstName, LastName FROM ${PREFIX}Users WHERE LastName LIKE ? OR FirstName LIKE ?; `, [`%${q}%`, `%${q}%`]);
 
 module.exports = { getAll, get, add, update, remove, getTypes, register, login, search, Types }
