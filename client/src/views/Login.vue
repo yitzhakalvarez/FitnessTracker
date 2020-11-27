0ced816fb1eccfbe392c130134d274a474682242
Yitzhak A <yitzhak.alvarez1998@yahoo.com>
2020-11-27 01:02:54 -0500

    Media Queries added

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -3,7 +3,7 @@
     <div class="hero-body">
       <div class="container">
         <div class="columns is-centered">
-          <div class="column is-5-tablet is-4-desktop is-3-widescreen">
+          <div class="column is-5-tablet is-5-desktop is-3-widescreen">
             <form action="" class="box" @submit.prevent="login">
               <div class="field">
                 <label for="" class="label">Username</label>
