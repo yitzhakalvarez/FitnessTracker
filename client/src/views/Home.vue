4ce73a66729b168bd0461198fff0f036d07d4d12
Yitzhak A <yitzhak.alvarez1998@yahoo.com>
2020-11-27 00:57:35 -0500

    Added Media Queries

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -45,4 +45,135 @@ export default {
   text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.7);
   font-size: 30px;
 }
+
+/* ----------- iPhone 4 and 4S ----------- */
+
+/* Portrait and Landscape */
+@media only screen 
+  and (min-device-width: 320px) 
+  and (max-device-width: 480px)
+  and (-webkit-min-device-pixel-ratio: 2) {
+  .hero {
+  text-align: center;
+  background-image: url("../assets/arnold.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 500px;
+}
+.hero-body .title {
+  text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.6);
+  padding: 40px 0 20px 0;
+  font-size: 60px;
+}
+.subtitle {
+  text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.7);
+  font-size: 30px;
+}
+}
+
+/* ----------- iPhone 5, 5S, 5C and 5SE ----------- */
+
+/* Portrait and Landscape */
+@media only screen 
+  and (min-device-width: 320px) 
+  and (max-device-width: 568px)
+  and (-webkit-min-device-pixel-ratio: 2) {
+  .hero {
+  text-align: center;
+  background-image: url("../assets/arnold.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 450px;
+}
+.hero-body .title {
+  text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.6);
+  padding: 40px 0 20px 0;
+  font-size: 35px;
+}
+.subtitle {
+  text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.7);
+  font-size: 22px;
+}
+}
+
+/* ----------- iPhone 6, 6S, 7 and 8 ----------- */
+
+/* Portrait and Landscape */
+@media only screen 
+  and (min-device-width: 375px) 
+  and (max-device-width: 667px) 
+  and (-webkit-min-device-pixel-ratio: 2) 
+  {
+    .hero {
+      text-align: center;
+      background-image: url("../assets/arnold.jpg");
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+      height: 500px;
+      }
+    .hero-body .title {
+      text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.6);
+      padding: 40px 0 20px 0;
+      font-size: 50px;
+      }
+    .subtitle {
+      text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.7);
+      font-size: 25px;
+      }
+  }
+
+/* ----------- iPhone 6+, 7+ and 8+ ----------- */
+
+/* Portrait and Landscape */
+@media only screen 
+  and (min-device-width: 414px) 
+  and (max-device-width: 736px) 
+  and (-webkit-min-device-pixel-ratio: 3) {
+        .hero {
+      text-align: center;
+      background-image: url("../assets/arnold.jpg");
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+      height: 560px;
+      }
+    .hero-body .title {
+      text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.6);
+      padding: 50px 0 20px 0;
+      font-size: 55px;
+      }
+    .subtitle {
+      text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.7);
+      font-size: 25px;
+      }
+}
+
+/* ----------- iPhone X ----------- */
+
+/* Portrait and Landscape */
+@media only screen 
+  and (min-device-width: 375px) 
+  and (max-device-width: 812px) 
+  and (-webkit-min-device-pixel-ratio: 3) { 
+            .hero {
+      text-align: center;
+      background-image: url("../assets/arnold.jpg");
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+      height: 630px;
+      }
+    .hero-body .title {
+      text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.6);
+      padding: 50px 0 20px 0;
+      font-size: 59px;
+      }
+    .subtitle {
+      text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.7);
+      font-size: 30px;
+      }
+}
 </style>
