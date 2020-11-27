384794a4811f08801392c86e4211f43fe4d16efe
Yitzhak A <yitzhak.alvarez1998@yahoo.com>
2020-11-26 19:11:23 -0500

    Facebook/Google oauth

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -32,13 +32,17 @@
             <div class="field is-grouped">
             <div class="control">
               <button class="button is-primary is-medium" @click="login">Login</button>
-              </div>
+            </div>
+            <div class="control">
+              <button class="button is-primary is-medium" @click.prevent="fbLogin">FB-Login</button>
+            </div>
+            <div class="control">
+              <button class="button is-primary is-medium" @click.prevent="googleLogin">Google Login</button>
+            </div>
             <div class="control">
           <router-link to="/Register" class="button is-primary is-medium">Register?</router-link>
             </div>
-                      
           </div>
-          
           </form>
         </div>
       </div>
@@ -49,28 +53,65 @@
 
 <script>
 import { context } from "../models/context";
-export default {
-  data() {
+export default 
+{
+  data() 
+  {
     return {
       username: "",
       password: ""
-    };
+    }; 
   },
-  methods: {
-    login() {
-      if (context.login(this.username, this.password)) {
-        this.$buefy.toast.open({
+  methods: 
+  {
+    login() 
+    {
+      if (context.login(this.username, this.password)) 
+      {
+        this.$buefy.toast.open(
+          {
           message: "Successfully logged in",
           type: "is-success"
-        });
+        }
+        );
         this.$router.push("schedule");
-      } else {
+      } 
+      else 
+      {
         this.$buefy.toast.open({
           message: "Login failed",
           type: "is-danger"
-        });
+        })
+      }
+  },
+      fbLogin()
+      {
+        FB.login(function(authInfo){
+          console.log(authInfo);
+        }, { scope: 'public_profile,email'})
+      },
+      googleLogin()
+      {
       }
     }
   }
-};
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '385069659231960',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v9.0'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 </script>
