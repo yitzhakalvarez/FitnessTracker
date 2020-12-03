0b4426431734fe6639eaf3cc5f32b227a6c9918b
Yitzhak A <yitzhak.alvarez1998@yahoo.com>
2020-12-02 21:50:21 -0500

    changes

--- client/src/models/my-fetch.js
+++ client/src/models/my-fetch.js
@@ -1,21 +1,19 @@
   
 /* B"H
 */
-const API_ROOT = process.env.API_ROOT || '/';
-//if there is no such environment variable then use local host 3000
-export function myFetch(url, data = null, method = null)
-{   if(data){
-        return fetch(API_ROOT + url, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        headers: {
-          'Content-Type': 'application/json'
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: JSON.stringify(data) // body data type must match "Content-Type" header
-      }).then(x=> x.json())
-    }
-    else{
-        return fetch(API_ROOT + url  ).then(x=> x.json() )
-    }
+const API_ROOT = process.env.API_ROOT || "http://localhost:3000/";
+
+export function myFetch(url, data = null) {
+  if (data) {
+    return fetch(API_ROOT + url, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    }).then(x => x.json());
+  } else {
+    return fetch(API_ROOT + url).then(x => x.json());
+  }
 }
--- server/models/mysql.js
+++ server/models/mysql.js
@@ -2,22 +2,25 @@
     The file for providing the connections and mySql functionality to the rest of the project
 */
 const mysql = require("mysql");
+require("dotenv").config();
 
 const pool = mysql.createPool({
-    connectionLimit: 10, // default 10
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-  });
+  connectionLimit: 10, // default 10
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+});
 
 const query = (sql, binding) => {
-    return new Promise((resolve, reject) => {
-      pool.query(sql, binding, (err, result, fields) => {
-        if (err) reject(err);
-        resolve(result);
-      });
+  return new Promise((resolve, reject) => {
+    pool.query(sql, binding, (err, result, fields) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(result);
     });
-  };
-  
+  });
+};
+
 module.exports = { pool, query };
