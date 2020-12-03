f184c739b620d1ffc78fc93d21372ac0c248fd17
Yitzhak A <yitzhak.alvarez1998@yahoo.com>
2020-12-02 21:32:47 -0500

    ff

--- client/src/models/my-fetch.js
+++ client/src/models/my-fetch.js
@@ -1,8 +1,8 @@
   
 /* B"H
 */
-const API_ROOT = process.env.API_ROOT || 'http://localhost:3000';
-//if there is no such environment variable then use local host 3001
+const API_ROOT = process.env.API_ROOT || '/';
+//if there is no such environment variable then use local host 3000
 export function myFetch(url, data = null, method = null)
 {   if(data){
         return fetch(API_ROOT + url, {
